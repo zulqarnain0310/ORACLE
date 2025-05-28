8fbd54614c2f426bf616e04fb360cd471514cbfa
Zulqarnain Shaikh <you@example.com>
2025-05-28 18:17:24 +0530

    ACL SCRIPTS ZAIN

---
+++ ACL_SCRIPTS/MD_CODE_REGEX_DATA_TABLE.sql
@@ -0,0 +1,7 @@
+REM INSERTING into ACL_RBL_MISDB_PROD.MD_CODE_REGEX
+SET DEFINE OFF;
+Insert into ACL_RBL_MISDB_PROD.MD_CODE_REGEX (ID,REGEX,DESCRIPTION) values (1,'\#[A-Z1-9\@\#\_]','Temporary tables');
+Insert into ACL_RBL_MISDB_PROD.MD_CODE_REGEX (ID,REGEX,DESCRIPTION) values (2,'INSERT','Insert statements');
+Insert into ACL_RBL_MISDB_PROD.MD_CODE_REGEX (ID,REGEX,DESCRIPTION) values (3,'SELECT','Select statements');
+Insert into ACL_RBL_MISDB_PROD.MD_CODE_REGEX (ID,REGEX,DESCRIPTION) values (4,'UPDATE','Update Statements');
+Insert into ACL_RBL_MISDB_PROD.MD_CODE_REGEX (ID,REGEX,DESCRIPTION) values (5,'DELETE','Delete Statements');
